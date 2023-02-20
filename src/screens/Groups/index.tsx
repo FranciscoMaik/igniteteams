d4f18b6e89dec1e6ab95b232112fdc2626923a54
FranciscoMaik <chiconunes3081@gmail.com>
2023-02-20 16:46:01 -0300

    feat: adjust list items

--- src/screens/Groups/index.tsx
+++ src/screens/Groups/index.tsx
@@ -1,19 +1,27 @@
+import { useState } from "react";
+
 import { Header } from "@components/Header";
 import { HighLight } from "@components/HighLight";
 import { GroupCard } from "@components/GroupCard";
 
 import * as S from "./styles";
+import { FlatList } from "react-native";
 
 export function Groups() {
+  const [groups, setGroups] = useState<Array<string>>(["Galera do MSN", "Galera do Trabalho"]);
+
   return (
     <S.ContainerGroups>
       <Header />
 
       <HighLight title="Turmas" subtitle="jogue com a sua turma"/>
 
-      <GroupCard title="Galera do MSN" />
-      <GroupCard title="Galera do MSN" />
-      <GroupCard title="Galera do MSN" />
+      <FlatList
+        data={groups}
+        keyExtractor={item => item}
+        renderItem={({ item }) => <GroupCard title={item} />}
+      />
+
     </S.ContainerGroups>
   )
 }
