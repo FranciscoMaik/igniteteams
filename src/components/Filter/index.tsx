5ee69d1270a3a4d0e3cfe70466b97873e4e2d3e3
FranciscoMaik <chiconunes3081@gmail.com>
2023-02-21 11:42:44 -0300

    feat: create filter and list players

---
+++ src/components/Filter/index.tsx
@@ -0,0 +1,16 @@
+import { TouchableOpacityProps } from "react-native";
+
+import * as S from "./styles";
+
+type FilterProps = TouchableOpacityProps & S.FilterStyleProps & {
+  title: string;
+}
+
+
+export function Filter({title, isActive = false, ...rest}: FilterProps){
+  return (
+    <S.ContainerFilter isActive={isActive} {...rest}>
+      <S.Title>{title}</S.Title>
+    </S.ContainerFilter>
+  );
+}
--- src/screens/Players/index.tsx
+++ src/screens/Players/index.tsx
@@ -1,11 +1,18 @@
+import { useState } from "react";
+import { FlatList } from "react-native";
+
 import { Header } from "@components/Header";
 import { HighLight } from "@components/HighLight";
 import { ButtonIcon } from "@components/ButtonIcon";
 import { Input } from "@components/Input";
 
 import * as S from "./styles";
+import { Filter } from "@components/Filter";
 
 export function Players() {
+  const [team, setTeam] = useState("Time A");
+  const [players, setPlayers] = useState([]);
+
   return (
     <S.ContainerPlayers>
       <Header showBackButton />
@@ -20,8 +27,29 @@ export function Players() {
           placeholder="Nome da pessoa"
           autoCorrect={false}
         />
-        <ButtonIcon icon="add"/>
+        <ButtonIcon icon="add" />
       </S.Form>
+
+      <S.HeaderList>
+        <FlatList
+          data={["Time A", "Time B"]}
+          keyExtractor={item => item}
+          renderItem={({ item }) => (
+            <Filter
+              title={item}
+              isActive={item === team}
+              onPress={() => setTeam(item)}
+            />
+          )}
+          horizontal
+        />
+
+        <S.NumbersOfPlayers>
+          {players.length}
+        </S.NumbersOfPlayers>
+      </S.HeaderList>
+
+
 
     </S.ContainerPlayers>
   );
