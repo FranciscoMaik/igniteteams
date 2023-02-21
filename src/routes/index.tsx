40670672281528ecfcc8faca91b828b3fdb1ca97
FranciscoMaik <chiconunes3081@gmail.com>
2023-02-21 16:58:14 -0300

    feat: resolve glitch

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -1,11 +1,17 @@
 import { NavigationContainer } from "@react-navigation/native";
+import { useTheme } from "styled-components/native";
+import { View } from "react-native";
 
 import { AppRoute } from "./app.routes";
 
-export function Routes(){
-  return(
-    <NavigationContainer>
-      <AppRoute />
-    </NavigationContainer>
+export function Routes() {
+  const { COLORS } = useTheme();
+
+  return (
+    <View style= {{flex: 1, backgroundColor: COLORS.GRAY_600 }}>
+      <NavigationContainer>
+        <AppRoute />
+      </NavigationContainer>
+    </View>
   );
 }
