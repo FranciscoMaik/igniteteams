230476c097f21054cb73d76cfb144fa21c143ec1
FranciscoMaik <chiconunes3081@gmail.com>
2023-02-21 16:20:09 -0300

    feat: craete types for navigation

--- App.tsx
+++ App.tsx
@@ -4,9 +4,7 @@ import { useFonts, Roboto_400Regular, Roboto_700Bold } from "@expo-google-fonts/
 
 import { Loading } from "@components/Loading";
 
-import { Groups } from '@screens/Groups';
-import { NewGroup } from "@screens/NewGroup";
-import { Players } from "@screens/Players";
+import { Routes } from "@routes/index";
 
 import theme from "./src/theme";
 
@@ -20,7 +18,7 @@ export default function App() {
         backgroundColor="transparent"
         translucent
       />
-      { fontsLoaded ? <Players /> : <Loading />}
+      { fontsLoaded ? <Routes /> : <Loading />}
     </ThemeProvider>
   );
 }
